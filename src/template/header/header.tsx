9df43d6c1bb1dc9278dbbe4b3d92bbbc3d97bb97
knighthat <knightchanelgaming@gmail.com>
2024-04-01 16:45:40 -0500

    page header

---
+++ src/template/header/header.tsx
@@ -0,0 +1,24 @@
+import './header.css'
+
+export default function Header() {
+    return (
+        <header>
+            <div className={'pure-u-1-4'}></div>
+            <div className={'pure-u-1-2 search-bar-alignment'}>
+                <div className={'search-bar-container'}>
+                    <input id={'search-bar'} placeholder="Search"/>
+                    <hr/>
+                </div>
+            </div>
+            <div className={'pure-u-1-4'}>
+                <nav>
+                    <ul>
+                        <li>My website</li>
+                        <li>Theme</li>
+                        <li>Changelogs</li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
