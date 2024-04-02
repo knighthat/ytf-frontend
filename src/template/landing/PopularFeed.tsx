b5507576c8a7f7c914fc78210dab5e6dbbd4eb49
knighthat <knightchanelgaming@gmail.com>
2024-04-01 19:49:57 -0500

    fetch and display popular videos

---
+++ src/template/landing/PopularFeed.tsx
@@ -0,0 +1,39 @@
+import {plainToInstance} from 'class-transformer';
+import React, {useEffect, useState} from "react";
+
+import './preview-card.css'
+
+import PreviewCard from "../../api/PreviewCard";
+
+async function fromBackend(): Promise<Array<PreviewCard>> {
+  try {
+    const request = await fetch(`${import.meta.env.VITE_API_ENDPOINT}/v1/popular?max=20`);
+    if (request.ok) {
+      const data = await request.json();
+      return plainToInstance(PreviewCard, data)
+    }
+  } catch (err) {
+    console.log(err)
+  }
+  return []
+}
+
+export default function PopularFeed() {
+
+  const [cards, setCards] = useState<JSX.Element[]>([]);
+  useEffect(() => {
+    const cardArray = async () => {
+      const backend = await fromBackend();
+      setCards(backend.map(video => video.toHTML()));
+    }
+
+    cardArray();
+  }, []);
+
+
+  return (
+      <div id='trending-feed' className={"pure-g"}>
+        {cards.map(element => (<React.Fragment>{element}</React.Fragment>))}
+      </div>
+  )
+}
