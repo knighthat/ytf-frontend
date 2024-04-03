8f2750c6dee56a3a1111f9130c253e58b057bc51
knighthat <knightchanelgaming@gmail.com>
2024-04-03 16:28:22 -0500

    implemented new backend.tsx template

--- src/template/landing/PopularFeed.tsx
+++ src/template/landing/PopularFeed.tsx
@@ -1,39 +1,20 @@
-import {plainToInstance} from 'class-transformer';
 import React, {useEffect, useState} from "react";
 
-import './preview-card.css'
+import '../../assets/css/preview-card.css'
 
 import {VideoPreviewCard} from "../../api/PreviewCard";
-
-async function fromBackend(): Promise<Array<VideoPreviewCard>> {
-  try {
-    const request = await fetch(`${import.meta.env.VITE_API_ENDPOINT}/v1/popular?max=20`);
-    if (request.ok) {
-      const data = await request.json();
-      return plainToInstance(VideoPreviewCard, data)
-    }
-  } catch (err) {
-    console.log(err)
-  }
-  return []
-}
+import {PopularVideos} from "../../api/backend";
 
 export default function PopularFeed() {
-
-  const [cards, setCards] = useState<JSX.Element[]>([]);
+  const [videos, setVideos] = useState<VideoPreviewCard[]>([]);
   useEffect(() => {
-    const cardArray = async () => {
-      const backend = await fromBackend();
-      setCards(backend.map(video => video.toHTML()));
-    }
-
-    cardArray();
+    const getVideos = async () => setVideos(await PopularVideos());
+    getVideos();
   }, []);
-
 
   return (
       <div id='trending-feed' className={"pure-g"}>
-        {cards.map(element => <React.Fragment key={element.key}>{element}</React.Fragment>)}
+        {videos.map((card, index) => <React.Fragment key={index}>{card.toHtml()}</React.Fragment>)}
       </div>
   )
 }
