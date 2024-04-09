f3f238211d2bdf00b1ed5d1751e2228ab3884909
knighthat <knightchanelgaming@gmail.com>
2024-04-08 19:54:33 -0500

    removed 'footer-margin' div

--- src/template/footer/footer.tsx
+++ src/template/footer/footer.tsx
@@ -52,43 +52,41 @@ function GetVersion() {
 export default function Footer() {
   return (
       <footer>
-        <div className={'footer-margin'}>
-          <div className={'pure-g'}>
-            <section className={'pure-u-1 pure-u-lg-1-3 footer-section'}>
-              <h2>What is this?</h2>
-              <hr/>
-              <ul>
-                <MakeLi icon='code-working-sharp' isIcon={true} text='YTF Project'/>
-                <MakeLi icon='logo-github' isIcon={true} text='Source code'/>
-                <MakeLi icon='mail-sharp' isIcon={true} text='Contact'/>
-              </ul>
-            </section>
-            <section className={'pure-u-1 pure-u-lg-1-3 footer-section'}>
-              <h2>Documentation</h2>
-              <hr/>
-              <ul>
-                <MakeLi icon='folder-open-sharp' isIcon={true} text='Documents'/>
-                <MakeLi icon='help-circle-sharp' isIcon={true} text='FAQ'/>
-                <MakeLi icon='layers-sharp' isIcon={true} text='Dependency licences' to='/licenses'/>
-              </ul>
-            </section>
-            <section className={'pure-u-1 pure-u-lg-1-3 footer-section'}>
-              <h2>Support</h2>
-              <hr/>
-              <ul>
-                <MakeLi icon='btc' isIcon={false} text='GpZUvhdVkCFxqF4vuaiFxtBTxB3L94vnK'/>
-                <MakeLi icon='xmr' isIcon={false} text='4G5dUFTbFWYfZAk69irnDUCTNZzP5viNTmkGpCNCLHjjYQq7jjRTWoYTH1aY8P841hExP31Qxg2hXPtxreKu11zCYdxQUj'/>
-                <MakeLi icon='eth' isIcon={false} text='x15b9F199b4Db223Ac35bfd5c43B7036E29d06141'/>
-              </ul>
-            </section>
-          </div>
-          <hr/>
-          <div className={'pure-g'}>
+        <div className={'pure-g'}>
+          <section className={'pure-u-1 pure-u-lg-1-3 footer-section'}>
+            <h2>What is this?</h2>
+            <hr/>
+            <ul>
+              <MakeLi icon='code-working-sharp' isIcon={true} text='YTF Project'/>
+              <MakeLi icon='logo-github' isIcon={true} text='Source code'/>
+              <MakeLi icon='mail-sharp' isIcon={true} text='Contact'/>
+            </ul>
+          </section>
+          <section className={'pure-u-1 pure-u-lg-1-3 footer-section'}>
+            <h2>Documentation</h2>
+            <hr/>
+            <ul>
+              <MakeLi icon='folder-open-sharp' isIcon={true} text='Documents'/>
+              <MakeLi icon='help-circle-sharp' isIcon={true} text='FAQ'/>
+              <MakeLi icon='layers-sharp' isIcon={true} text='Dependency licences' to='/licenses'/>
+            </ul>
+          </section>
+          <section className={'pure-u-1 pure-u-lg-1-3 footer-section'}>
+            <h2>Support</h2>
+            <hr/>
+            <ul>
+              <MakeLi icon='btc' isIcon={false} text='GpZUvhdVkCFxqF4vuaiFxtBTxB3L94vnK'/>
+              <MakeLi icon='xmr' isIcon={false} text='4G5dUFTbFWYfZAk69irnDUCTNZzP5viNTmkGpCNCLHjjYQq7jjRTWoYTH1aY8P841hExP31Qxg2hXPtxreKu11zCYdxQUj'/>
+              <MakeLi icon='eth' isIcon={false} text='x15b9F199b4Db223Ac35bfd5c43B7036E29d06141'/>
+            </ul>
+          </section>
+        </div>
+        <hr/>
+        <div className={'pure-g'}>
             <span id={'copyright-notice'} className={'pure-u-1 pure-u-lg-1-2'}>
               Copyright © 2024 Knight Hat. All Rights Reserved.
             </span>
-            <GetVersion/>
-          </div>
+          <GetVersion/>
         </div>
       </footer>
   )
